51b4ba9c629323c4f621a275866ab56499bce7c5
林昌賜 <brian29688071@gmail.com>
2020-11-23 19:12:05 +0800

    no message

--- PlayMp3FromPROGMEM/PlayMp3FromPROGMEM.ino
+++ PlayMp3FromPROGMEM/PlayMp3FromPROGMEM.ino
@@ -1,42 +1,93 @@
+/*先取mqtt的封包放進byte[]然後丟入*/
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 #include <Arduino.h>
-#ifdef ESP32
-    #include <WiFi.h>
-#else
-    #include <ESP8266WiFi.h>
-#endif
-
 #include "AudioFileSourcePROGMEM.h"
-#include "AudioGeneratorWAV.h"
+#include "AudioGeneratorMP3.h"
 #include "AudioOutputI2SNoDAC.h"
-
-// VIOLA sample taken from https://ccrma.stanford.edu/~jos/pasp/Sound_Examples.html
-#include "viola.h"
-
-AudioGeneratorWAV *wav;
+#include "string.h"
+#include "stdlib.h"
+#include <SPI.h>
+#include "ESP8266Spiram.h"
+const char *ssid = "MSHOME";
+const char *password = "0226919072";
+const char *mqtt_server = "192.168.0.103";
+const char *myname = "myname";       //topic
+const char *mynowplay = "mynowplay"; //topic
+WiFiClient espClient;
+PubSubClient client(espClient);
 AudioFileSourcePROGMEM *file;
+AudioGeneratorMP3 *mp3;
 AudioOutputI2SNoDAC *out;
-
+ESP8266Spiram spiram;
+void setup_wifi()
+{
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  randomSeed(micros());
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+void callback(char *topic, byte *payload, unsigned int length) //接收回傳
+{
+  Serial.println(length);
+  for (int i = 0; i < length; i++)
+  {
+    
+  }
+}
+void reconnect()
+{
+  // Loop until  reconnected
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    String clientId = "ESP8266Clinet-";
+    clientId += String(random(0xffff), HEX);
+    if (client.connect(clientId.c_str()))
+    {
+      Serial.println("connected");
+      client.subscribe(myname);
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
 void setup()
 {
-  WiFi.mode(WIFI_OFF); 
   Serial.begin(115200);
-  delay(1000);
-  Serial.printf("WAV start\n");
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  spiram.begin();
 
   audioLogger = &Serial;
-  file = new AudioFileSourcePROGMEM( viola, sizeof(viola) );
   out = new AudioOutputI2SNoDAC();
-  wav = new AudioGeneratorWAV();
-  wav->begin(file, out);
+  mp3 = new AudioGeneratorMP3();
 }
 
 void loop()
 {
-  if (wav->isRunning()) {
-    if (!wav->loop()) wav->stop();
-  } else {
-    Serial.printf("WAV done\n");
-    delay(1000);
+  if (!client.connected())
+  {
+    reconnect();
   }
+  client.loop();
+  
 }
-
