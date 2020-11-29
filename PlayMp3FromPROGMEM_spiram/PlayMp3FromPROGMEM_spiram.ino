397252039001e10f12a7a40aa36c6717e72d7c70
林昌賜 <brian29688071@gmail.com>
2020-11-29 19:54:45 +0800

    轉換方法先驗證撥放能力-直接從buffer撥放

--- PlayMp3FromPROGMEM_spiram/PlayMp3FromPROGMEM_spiram.ino
+++ PlayMp3FromPROGMEM_spiram/PlayMp3FromPROGMEM_spiram.ino
@@ -1,3 +1,4 @@
+/*先取mqtt的封包放進byte[]然後丟入*/
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <Arduino.h>
@@ -6,6 +7,7 @@
 #include "AudioOutputI2SNoDAC.h"
 #include "string.h"
 #include "stdlib.h"
+#include <SPI.h>
 #include "ESP8266Spiram.h"
 const char *ssid = "MSHOME";
 const char *password = "0226919072";
@@ -17,6 +19,7 @@ PubSubClient client(espClient);
 AudioFileSourcePROGMEM *file;
 AudioGeneratorMP3 *mp3;
 AudioOutputI2SNoDAC *out;
+ESP8266Spiram *spiram;
 void setup_wifi()
 {
   delay(10);
@@ -38,7 +41,13 @@ void setup_wifi()
 }
 void callback(char *topic, byte *payload, unsigned int length) //接收回傳
 {
- 
+  byte test[length];
+  Serial.println(length);
+  //if(topic=="mp3_frame_byte")
+    spiram->write(0x00,payload,length);
+  spiram->read(0x00,test,length);
+  for(int i=0;i<length;i++)
+    Serial.println((String)(char)test[i]); 
 }
 void reconnect()
 {
@@ -50,7 +59,12 @@ void reconnect()
     clientId += String(random(0xffff), HEX);
     if (client.connect(clientId.c_str()))
     {
+      byte b[1]={0x07};
+      byte c[1];
+      spiram->write(0x00,b,1);
+      spiram->read(0x00,c,1);
       Serial.println("connected");
+      Serial.println(c[0]);
       Serial.println(client.subscribe(mp3_frame_byte));
       Serial.println(client.subscribe(mynowplay));
       Serial.println(client.publish("online", "online"));
@@ -70,6 +84,9 @@ void setup()
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
+  spiram  = new ESP8266Spiram(4, 40e6);
+  spiram->begin();
+  spiram->setByteMode();
   audioLogger = &Serial;
   out = new AudioOutputI2SNoDAC();
   mp3 = new AudioGeneratorMP3();
